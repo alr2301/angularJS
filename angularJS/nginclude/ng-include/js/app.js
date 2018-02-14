37aa480fcad99ca05c5a26f286ff0b483742ca65
alr2301 <alr23011982@gmail.com>
2018-02-14 14:36:58 +0100

    Commit incial del ng-include

--- angularJS/nginclude/ng-include/js/app.js
+++ angularJS/nginclude/ng-include/js/app.js
@@ -7,11 +7,13 @@ var app = angular.module('ejemplosApp',[ ]);
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
   
-	$scope.profesores={};
+	$scope.profesores = {};
 
+  
   	$http.get('json/profesores.json').success(function(data){
-  		//codigo cuando es correcta la peticion
-  			$scope.profesores=data.profesores;
+  		//Codigo cuando es correcta la petición
+  		$scope.profesores = data.profesores;
+
   	});
 
 
